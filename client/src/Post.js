7cde8d5485685fc6a93acda378c9fedcc341bd5a
Subhrajit <subhrajitmukh1234@gmail.com>
2023-08-21 20:40:28 +0530

    try

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,5 +1,6 @@
 import {formatISO9075} from "date-fns";
 import {Link} from "react-router-dom";
+const url=process.env.REACT_APP_URL;
 
 export default function Post({_id,title,summary,cover,content,createdAt,author}) {
 
@@ -7,7 +8,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'http://localhost:4000/'+cover} alt=""/>
+          <img src={`${url}/`+cover} alt=""/>
         </Link>
       </div>
       <div className="texts">
